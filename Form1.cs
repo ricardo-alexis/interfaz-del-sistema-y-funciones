77b6464f70ded440a6fc6bc9ba0ebf428360aef0
asalmero <71348959+asalmero@users.noreply.github.com>
2020-10-23 21:16:27 -0500

    Se requere Actualizacion

--- Form1.cs
+++ Form1.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using MySql.Data.MySqlClient;
-
+//TRABAJO EXCELENTE
 namespace Interfaz_del_sistema
 {
     public partial class Form1 : Form
